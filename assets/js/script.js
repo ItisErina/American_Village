f4b816848c1757b219172eac8915f25430059f03
DESKTOP-NET3VUU\mm_an <eri_matsuo@icloud.com>
2021-11-11 20:19:36 +0900

    Swiper

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+let mySwiper6 = new Swiper ('#swiper06', {
+    autoplay: {
+      delay: 2000,
+    },
+    
+    loop: true,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });

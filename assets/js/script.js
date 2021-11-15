ca5697da3f81d541edcfc3cdd84fb48d689cca28
DESKTOP-NET3VUU\mm_an <eri_matsuo@icloud.com>
2021-11-15 19:14:39 +0900

    TopPage edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
+let mySwiper06 = new Swiper ('#swiper06', {
+  autoplay: {
+    delay: 2000,
+  },
+  
+  loop: true,
+  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-let mySwiper6 = new Swiper ('#swiper06', {
-    autoplay: {
-      delay: 2000,
-    },
-    
-    loop: true,
-    
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    // },
-    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+console.log(mySwiper06);
